9fea22857336f6c21fb9a6bf610a4f95d1a05624
Tomas Dahlander <thomaz.dahlander@hotmail.com>
2021-05-28 11:29:37 +0200

    Added Json file for later implement database

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
 // Global variables ********************************************************************************************************
 
-let questions = [
-    "Hur skulle du beskriva dig själv?<br><br>Kreativ, Logisk eller Analytisk?",
-    "Vad är din favorit druva och region?",
-    "Du står i kön på ICA och en person tränger sig före i kön.<br><br>Vad gör du?",
-    "Hur många enheter av öl/vin har du druckit hittills i år?",
-    "Hur många koppar kaffe har du druckit hittills i år?",
-    "Om du bara fick dricka två drycker för resten av livet där den ena är vatten, vad skulle den andra vara?",
-    "Om du bara fick förtära en av följande resten av livet, vad väljer du?<br><br>Ost, Vin eller Choklad.",
-    "Om du var tvungen att göra något av följande, vad skulle du göra?<br><br>Snagga dig, Pierca dig i nästan eller Valfri tatuering (minst 5 cm i diameter).",
-    "Om du bara fick klä dig en viss färg på överkroppen för de nästkommande 5 åren, vilken färg väljer du?<br><br>Röd, Grön eller Blå.",
-    "Vad är det viktigaste tillbehören på en korv med bröd?",
-    "Vilken hushållssyssla slipper du helst?",
-    "Vilket resemål har varit det mest minnesvärde?",
-    "Vilken sport/idrott tror du att du har störst chans att vinna i över samtliga i detta sällskap?",
-    "Hur många hårfärger har du haft genom livet?",
-    "Vilken kroppsdel är du mest nöjd över?",
-    "Vad lockar mest?<br><br>Husvagnssemester, Charterresa eller Fjällvandring.",
-    "Vilket årtionde har tillfört mest/bäst till musikvärlden?",
-    "Vilken diagnos tror du att du ligger närmast?<br><br>Autism, tvångssyndrom/OCD eller tourettes.",
-    "I vilken miljö ser du dig bo om 10 år?<br><br>Stadsmiljö, förort eller landsbygd?",
-    "Vad är det viktigaste pålägget på pizzan förutom tomatsås och ost?",
-    "Nästa vecka får du bara äta en och samma rätt till lunch och middag.<br><br>Vad blir det?",
-    "Om du inte behövde ta hänsyn till livspusslet, vilket husdjur skulle du då ha?",
-    "Vilket är det absolut sötaste djuret?",
-    "Vad är din favoritblomma?",
-    "Du får in en maträtt på en resturang som inte uppfyller dina förväntningar.<br><br>Vad gör du?",
-    "Vilken är den bästa åldern?",
-    'Vad skulle du säga är ditt "spirit animal"',
-    "Du sitter på ett flygplan till ett främmande land på semester.<br><br>Vad ser du mest fram emot?",
-    "Om du magiskt skulle få lära dig ett nytt instrument imorgon, vilket skulle det vara?",
-    "Vilket ytterligare språk än dom du kan skulle du vilja kunna prata flytande?",
-    "Om du hade fått välja ett annat yrke i ditt liv, vad hade det varit?",
-    "Du är på ett kalas med ett nytt umgänge och det finns bara en tårtbit kvar som du är sugen på.<br><br>Vad gör du?",
-];
+let questions;
 
 let counter;
 
@@ -48,7 +15,7 @@ $(document).ready(function () {
      */
     $("#nextQuestionBtn").click(function () {
         if (counter != questions.length) {
-            $("#questionArea").html(questions[counter]);
+            $("#questionArea").html(questions[counter].question);
             $("#currentQuestion").html(counter + 1);
             counter++;
         }
@@ -61,7 +28,7 @@ $(document).ready(function () {
     $("#previousQuestionBtn").click(function () {
         if (counter > 1) {
             counter--;
-            $("#questionArea").html(questions[counter - 1]);
+            $("#questionArea").html(questions[counter - 1].question);
             $("#currentQuestion").html(counter);
         }
     });
@@ -98,8 +65,7 @@ $(document).ready(function () {
     function shuffle() {
         const amountOfQuestions = questions.length;
         for (let i = 0; i < questions.length; i++) {
-            const randomQuestionIndex =
-                getRandomQuestionIndex(amountOfQuestions);
+            const randomQuestionIndex = getRandomQuestionIndex(amountOfQuestions);
             const temp = questions[i];
             questions[i] = questions[randomQuestionIndex];
             questions[randomQuestionIndex] = temp;
@@ -115,13 +81,28 @@ $(document).ready(function () {
         return (nr = Math.floor(Math.random() * Math.floor(max)));
     }
 
-    // Runs when loaded ****************************************************************************************************
+    /**
+     * Fetches the questions array from a JSON file
+     */
+    function getQuestionsArray() {
+        fetch("/questions.json")
+            .then((resp) => resp.json())
+            .then((data) => setQuestions(data));
+    }
 
-    counter = 0;
-    modal = $("#rules-modal");
+    /**
+     * Receives JSON data and sets the questions array and calls shuffle
+     * @param {JSON data} questionsDataArray
+     */
+    function setQuestions(questionsDataArray) {
+        questions = questionsDataArray;
+        shuffle();
+    }
+
+    // Runs when loaded ****************************************************************************************************
+    getQuestionsArray(); // Gets the questions
+    counter = 0; // initiates the questions counter
     $("#currentQuestion").html(counter);
-    $("#indexfooter").html(
-        `Copyright &copy; ${new Date().getFullYear()} – Tomas Dahlander`
-    );
-    shuffle();
+    modal = $("#rules-modal");
+    $("#indexfooter").html(`Copyright &copy; ${new Date().getFullYear()} – Tomas Dahlander`); // set the current year in footer
 });
